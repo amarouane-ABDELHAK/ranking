df56b12b986c3f25b962fdbecb8508219dde8944
amarouane <amarouane@itsc.uah.edu>
2019-08-07 21:47:46 -0500

    Interview

--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -26,39 +26,7 @@ export class RankingComponent implements OnInit {
   rank_a_list() {
   
     
-    this.dataRanks.push({name: this.dataSubmitted.name, score: this.dataSubmitted.rank })
-
-    this.dataRanks.sort(function(first, second) {
-      return second.score - first.score;
-     });
-    let all_scores = []
-    this.dataRanks.forEach(element => {
-      all_scores.push(element['score'])
-    });
-    let uniqueScores = all_scores.filter(function(elem, pos) {
-      return all_scores.indexOf(elem) == pos;
-  })
-  
-  this.ids = this.initArray(all_scores.length)
-  console.log(this.ids)
-  var _this = this
-
-  all_scores.forEach(function (value, i) {
-    uniqueScores.forEach(element => {
-      if(value < element) {
-        _this.ids[i] += 1
-        return
-      }
-      
-    });
-    
-});
-  //   for i, n_2 in enumerate(list_to_be_ranked):
-  //       for n_1 in temp_list:
-  //           if n_2 < n_1:
-  //               rank[i] += 1
-  //               continue
-  //   return rank
+    //YOur code here
 
 
 
