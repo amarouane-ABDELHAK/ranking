bc7e62be17972b5c06ca04834eaa12dc8f5f3cda
amarouane <amarouane@itsc.uah.edu>
2019-08-07 21:50:47 -0500

    Interview

---
+++ rank_a_list.py
@@ -0,0 +1,10 @@
+
+def rank_a_list(original_list, list_to_be_ranked):
+    rank = []
+    #Your code here
+    return rank
+
+
+#Example
+# rank_a_list([100, 90, 90, 80, 75, 60], [50, 65, 77, 90, 102])
+# will return [6, 5, 4, 2, 1]
